090381a3d1a85ea1f3745bf798758c55174fc01e
Khoi Nguyen <knguyen@statestitle.com>
2020-09-21 23:46:11 -0700

    Modify start script in package.json

--- server.js
+++ server.js
@@ -1,16 +1,4 @@
 'use strict';
 
 const unleash = require('./lib/server-impl');
-const enableAuth0auth = require('./examples/auth0-auth-hook');
-
-unleash
-    .start({
-        adminAuthentication: 'custom',
-        preRouterHook: enableAuth0auth,
-    })
-    .then(server => {
-        // eslint-disable-next-line no-console
-        console.log(
-            `Unleash started on http://localhost:${server.app.get('port')}`,
-        );
-    });
+unleash.start()
