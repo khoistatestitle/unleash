f490ce3f053d0ce9ae4b3ee945f9d7bb299def16
Khoi Nguyen <knguyen@statestitle.com>
2020-09-21 23:23:23 -0700

    Use Auth0

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 'use strict';
 
 const unleash = require('./lib/server-impl');
+const enableAuth0auth = require('./examples/auth0-auth-hook');
 
-unleash.start();
+unleash
+    .start({
+        adminAuthentication: 'custom',
+        preRouterHook: enableAuth0auth,
+    })
+    .then(server => {
+        // eslint-disable-next-line no-console
+        console.log(
+            `Unleash started on http://localhost:${server.app.get('port')}`,
+        );
+    });
